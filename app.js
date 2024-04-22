5f961138605066b44bd1d1c0c22eb7e050a586e0
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 15:39:45 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // write code here
+<script>
+        function sumSalaries(obj) {
+            let totalSalaries = 0;
+            for (let key in obj) {
+              totalSalaries += obj[key];
+            }
+            return totalSalaries;
+          }
+          
+          let salaries = {
+            John: 100,
+            Ann: 160,
+            Pete: 130
+    
+          };
+          
+          console.log(sumSalaries(salaries)); // 390
+          
+    </script>
